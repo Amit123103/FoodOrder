55b9b730699b139ba373d94d8adb1eb9e02e3fc4
Amit kumar <amitakhil001@gmail.com>
2026-06-03 11:55:32 +0530

    Allow new Firebase UID for Owner login

--- src/pages/OwnerLogin.jsx
+++ src/pages/OwnerLogin.jsx
@@ -49,7 +49,12 @@ const OwnerLogin = ({ setIsOwnerLoggedIn, setCurrentPage }) => {
     try {
       const result = await confirmationResult.confirm(otpInput);
       // Validate owner UID
-      if (result.user.uid === 'hFd2WOQr7pd1gdYl1Ace3gyOHku1') {
+      const validUids = [
+        'hFd2WOQr7pd1gdYl1Ace3gyOHku1',
+        'AdpetEZgy4tpc08xw7b9yB4alhSMWkTsE9mP-TdlrOxxa6vcu2PykUVXO43ZezrtIKEEOclLOzLu_5HjUySLt3z0GCHhUp-3kgQ_5A2yRy50O8hruMr91-kEl19Sq9Xc77lB52NpLvZPDKUlb1NSsxMMRA'
+      ];
+      
+      if (validUids.includes(result.user.uid)) {
         setIsOwnerLoggedIn(true);
         setCurrentPage('owner_dashboard');
       } else {
